30fcfb44f928f0f9dcf16f7d0c20e06914634e21
Jonas Eschenburg <jonas@tnlgame.net>
2007-05-18 16:27:45 +0200

    Added upload script
    darcs-hash:20070518142745-d7774-34ada31ba0f1822bd8d8be159de4ebc369cb7239.gz

---
+++ upload-to-server.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo -n "Enter FTP server username: "
+read user
+
+cmd="mirrordir -v --exclude ./fonts --exclude ./lod-terrains --exclude ./models --exclude ./scripts --exclude ./sounds --exclude ./textures --exclude ./_darcs/current . ftp://$user@tnlgame.net/darcs/TnL-data"
+
+echo -e "Will execute:\n\n  $cmd\n"
+echo "Ctrl-C to abort, ENTER to continue."
+read
+
+echo $cmd
+
+exec $cmd
+
