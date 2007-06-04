677e056405e9dc002d1ea01628121153afcecd6d
Jonas Eschenburg <jonas@tnlgame.net>
2007-06-04 21:13:37 +0200

    Server upload script reflects new directory layout
    darcs-hash:20070604191337-d7774-4cc14d5e7057db2232818ed4d758725c874ac4f9.gz

--- upload-to-server.sh
+++ upload-to-server.sh
@@ -3,7 +3,7 @@
 echo -n "Enter FTP server username: "
 read user
 
-cmd="mirrordir -v --exclude ./fonts --exclude ./lod-terrains --exclude ./models --exclude ./scripts --exclude ./sounds --exclude ./textures --exclude ./_darcs/current . ftp://$user@tnlgame.net/darcs/TnL-data"
+cmd="mirrordir -v --exclude ./fonts --exclude ./lod-terrains --exclude ./models --exclude ./scripts --exclude ./sounds --exclude ./textures --exclude ./_darcs/current --exclude ./share . ftp://$user@tnlgame.net/darcs/TnL-data"
 
 echo -e "Will execute:\n\n  $cmd\n"
 echo "Ctrl-C to abort, ENTER to continue."
