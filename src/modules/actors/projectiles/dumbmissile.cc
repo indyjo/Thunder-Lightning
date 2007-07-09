14879e4cb072e3a579f0318e8935446d6cb0cc78
Jonas Eschenburg <jonas@tnlgame.net>
2007-07-09 04:01:15 +0200

    DumbMissile and SmartMissile2: Use configurability of Missile, economize lots of code
    darcs-hash:20070709020115-d7774-15abdea196f48aacdef2b5b2611c81eb820338cf.gz

--- src/modules/actors/projectiles/dumbmissile.cc
+++ src/modules/actors/projectiles/dumbmissile.cc
@@ -7,26 +7,10 @@
 // A dumb missile inspired by the Hydra 70 with data from
 // http://www.fas.org/man/dod-101/sys/missile/hydra-70.htm
 
-#define BLAST_BEGIN 0
-#define BLAST_END 1.05
-#define BLAST_THRUST 6007.0
-
-
 DumbMissile::DumbMissile(Ptr<IGame> thegame, Ptr<IActor>, Ptr<IActor> source)
-    : Missile(thegame, source)
+    : Missile(thegame, source, "DumbMissile")
 {
-    setTargetInfo(new TargetInfo("Dumb missile", 1.0f, TargetInfo::MISSILE));
-
-    float mass = 4.54; 
-    engine->construct(mass, 6, 6, 2);
-    thrust->setMaxForce(Vector(0,0,BLAST_THRUST));
-    
-    blast_begin = BLAST_BEGIN;
-    blast_end = BLAST_END;
-    min_explosion_age = 0.0f;
-    
-    setModel(thegame->getModelMan()->query(
-    	thegame->getConfig()->query("DumbMissile_model")));
+    // nothing to do anymore?
 }
 
 
--- src/modules/actors/projectiles/smartmissile2.cc
+++ src/modules/actors/projectiles/smartmissile2.cc
@@ -10,11 +10,6 @@
 
 #include "smartmissile2.h"
 
-#define BLAST_BEGIN 0.5
-#define BLAST_END 2.5
-#define BLAST_THRUST 74000.0
-#define MIN_EXPLOSION_AGE 1.5f
-#define MAX_LIFETIME 30.0f
 #define PI 3.14159265358979323846264338327f
 #define SCAN_ANGLE (120 * PI / 180.0)
 
@@ -26,29 +21,11 @@ http://www.chinfo.navy.mil/navpalib/factfile/missiles/wep-side.html
 
 
 SmartMissile2::SmartMissile2(Ptr<IGame> thegame, Ptr<IActor> target, Ptr<IActor> source)
-:   Missile(thegame, source), target(target)
+:   Missile(thegame, source, "SmartMissile"), target(target)
 {
-    blast_begin = BLAST_BEGIN;
-    blast_end = BLAST_END;
-    max_lifetime = MAX_LIFETIME;
-    min_explosion_age = MIN_EXPLOSION_AGE;
-
     setTargetInfo(new TargetInfo(
         "Interceptor Missile", 1.0f, TargetInfo::GUIDED_MISSILE));
-
-    float m = 85.5f;  // wheight in kg
-    float l = 2.89f;  // length in m
-    float r = 0.065f; // radius in m
-    float Iz = 0.5f*m*r*r; 			//  0.18062
-    float Ix = m*(l*l/12 + r*r/4);  // 59.59902
-
-    // As nice as real values are: the above ones bring numerical instability.
-    engine->construct(m, 60, 60, 20);
     engine->addEffector(new Effectors::MissileControl);
-    thrust->setMaxForce(Vector(0,0,BLAST_THRUST));
-    
-    setModel(thegame->getModelMan()->query(
-    	thegame->getConfig()->query("SmartMissile_model")));
 }
 
 
@@ -95,19 +72,12 @@ void SmartMissile2::interceptTarget(float delta_t) {
     Vector error = los_rotation;
     Vector d_error = d_error_dt.differentiate(error, delta_t);
     Vector I_error = I_error_dt.integrate(error, delta_t);
-    
-    //ls_message("missile %p acceleration angular: ", this); los_rotation.dump();
-    blast_begin = BLAST_BEGIN;
-    blast_end = BLAST_END;
-    max_lifetime = MAX_LIFETIME;
-    min_explosion_age = MIN_EXPLOSION_AGE;
-    
 
     Ptr<IConfig> config = thegame->getConfig();
     getControls()->setVector("angular_accel",
-        config->queryFloat("Missile_Kp",1)*error
-        + config->queryFloat("Missile_Ki",1)*I_error
-        + config->queryFloat("Missile_Kd",1)*d_error
+        config->queryFloat("SmartMissile_Kp",1)*error
+        + config->queryFloat("SmartMissile_Ki",1)*I_error
+        + config->queryFloat("SmartMissile_Kd",1)*d_error
         );
 }
 
