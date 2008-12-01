a98f61daf48df6478646e52132cddfc75136aa9d
Jonas Eschenburg <jonas@tnlgame.net>
2008-12-01 22:56:18 +0100

    Mirrordir script updated
    darcs-hash:20081201215618-f43ef-0cb863d635395d4dd0906ebabbb42e8576c87089.gz

--- upload-to-server.sh
+++ upload-to-server.sh
@@ -3,7 +3,7 @@
 echo -n "Enter FTP server username: "
 read user
 
-cmd="mirrordir -v --exclude ./_darcs/current --exclude ./build . ftp://$user@tnlgame.net/darcs/TnL-source"
+cmd="mirrordir -v -X ./_darcs/current -X ./build -X ./_darcs/pristine . ftp://$user@tnlgame.net/darcs/TnL-source"
 
 echo -e "Will execute:\n\n  $cmd\n"
 echo "Ctrl-C to abort, ENTER to continue."
